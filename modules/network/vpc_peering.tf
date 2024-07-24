29bae330bfd7be8c7e985f6619e753dcfdbf2523
praveen <praveen.dwivedi65@gmail.com>
2024-07-24 09:39:30 +0530

    add peering code

---
+++ modules/network/vpc_peering.tf
@@ -0,0 +1,18 @@
+data aws_vpc "default_vpc"{
+    tags = {
+        name = "Name"
+        values = "my-eks-vpc"
+      }
+}
+
+
+resource "aws_vpc_peering_connection" "foo" {
+  count = var.vpc_peering_required == true ? 1 : 0
+  peer_vpc_id   = data.aws_vpc.default_vpc.id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
+
+  tags = {
+    Name = "VPC Peering between default and main vpc"
+  }
+}
